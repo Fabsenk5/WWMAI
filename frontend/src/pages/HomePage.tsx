63e83feaadc044a841f6797fd013296c00131635
Fab K <fabiank5@hotmail.com>
2025-12-10 20:44:34 +0100

    Translate HomePage and add missing keys

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -2,14 +2,18 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './HomePage.css';
 
+import { useTranslation } from 'react-i18next';
+
 const HomePage: React.FC = () => {
+    const { t } = useTranslation();
+
     return (
         <div className="home-page">
-            <h1>Welcome to Who Wants to be a Millionaire!</h1>
-            <p>Choose an option to begin:</p>
+            <h1>{t('welcome_message')}</h1>
+            <p>{t('choose_option')}</p>
             <div className="button-container">
-                <Link to="/create-game" className="button">Create New Game</Link>
-                <Link to="/join" className="button">Join Game</Link>
+                <Link to="/create-game" className="button">{t('create_game')}</Link>
+                <Link to="/join" className="button">{t('join_game')}</Link>
             </div>
         </div>
     );
